123d12440084296cd1380f4bb0e020496bd7deff
Sidney Alex <silvercenturion2@gmail.com>
2022-05-22 23:39:58 -0300

    change nuxt config again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,10 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 
 export default defineNuxtConfig({
-    rootDir: './',
     target: 'static',
     css: ["~/assets/css/tailwind.css"],
     build: {
+        publicPath: './nuxt',
         followSymlinks: false,
         postcss: {
             postcssOptions: {
